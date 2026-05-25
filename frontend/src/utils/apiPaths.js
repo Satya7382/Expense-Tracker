496de4ae33e57128e0eec7ddebc31985add523ab
Satya Sri <satyakattamuri05@gmail.com>
2026-05-25 14:06:38 +0530

    base url change

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://expense-tracker-u09u.onrender.com';
+export const BASE_URL = 'https://expense-tracker-1-rvdt.onrender.com';
 
 export const API_PATHS = {
     AUTH: {
